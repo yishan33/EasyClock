79a233add8e2f2f66b4661364a8136f53112c56a
021159 <liufushan@htsc.com>
2022-10-14 18:50:26 +0800

    Initial Commit

---
+++ EasyClock/EasyClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyClockApp.swift
+//  EasyClock
+//
+//  Created by liufushan on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct EasyClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
